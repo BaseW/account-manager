442bd77932fef04035af5e581dfc5ae2098f4f02
wada <wadayugo@keio.jp>
2022-12-17 23:08:53 +0900

    import accounts from csv

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,57 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Account {
+    url: String,
+    username: String,
+    from: String,
+}
+
+fn parse_record_as_account(record: &csv::StringRecord, from: &str) -> Account {
+    // get necessary info
+    // if from is icloud, url is 1, username is 2
+    // if from is chrome, url is 1, username is 2
+    // if from is firefox, url is 0, username is 1
+    let url = match from {
+        "icloud" => record.get(1).unwrap().to_string(),
+        "chrome" => record.get(1).unwrap().to_string(),
+        "firefox" => record.get(0).unwrap().to_string(),
+        _ => panic!("unknown from"),
+    };
+    let username = match from {
+        "icloud" => record.get(2).unwrap().to_string(),
+        "chrome" => record.get(2).unwrap().to_string(),
+        "firefox" => record.get(1).unwrap().to_string(),
+        _ => panic!("unknown from"),
+    };
+    Account {
+        url,
+        username,
+        from: from.to_string(),
+    }
+}
+
+// return serialized accounts
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn import_accounts(csv_data: &str, from: &str) -> Vec<Account> {
+    // read csv data
+    let mut rdr = csv::Reader::from_reader(csv_data.as_bytes());
+    let mut accounts = Vec::new();
+    for result in rdr.records() {
+        let record = result.unwrap();
+        let account = parse_record_as_account(&record, from);
+        accounts.push(account);
+    }
+    // return serialized accounts
+    accounts
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![import_accounts])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,88 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import Image from "next/image";
 import reactLogo from "../assets/react.svg";
 import tauriLogo from "../assets/tauri.svg";
 import nextLogo from "../assets/next.svg";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+type Account = {
+  url: string;
+  username: string;
+  from: 'icloud' | 'chrome' | 'firefox'
+}
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+function App() {
+  const [isUploading, setIsUploading] = useState(false);
+  const [fileFrom, setFileFrom] = useState(''); // ['icloud', 'chrome', 'firefox'
+  const [csvData, setCsvData] = useState<string | ArrayBuffer | null>(null);
+  const [accounts, setAccounts] = useState<Account[]>([]);
+
+  function onUploadFile(e: ChangeEvent<HTMLInputElement>) {
+    setIsUploading(true);
+    const file = e.target.files[0];
+
+    const fileName = file.name;
+    let from = '';
+    // get type from file name
+    if (fileName.includes('icloud')) {
+      from = 'icloud';
+    } else if (fileName.includes('chrome')) {
+      from = 'chrome';
+    } else if (fileName.includes('firefox')) {
+      from = 'firefox';
+    }
+
+    if (from === '') {
+      setIsUploading(false);
+      return;
+    }
+
+    setFileFrom(from);
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const text = e.target?.result;
+      setIsUploading(false);
+      setCsvData(text);
+    };
+    reader.readAsText(file);
+  }
+
+  function startImport() {
+    console.log('start importing');
+    invoke("import_accounts", { csvData, from: fileFrom }).then((res) => {
+      const newlyImportedAccounts = res as Account[];
+      const concatenedAccounts = [...accounts, ...newlyImportedAccounts];
+      setAccounts(concatenedAccounts);
+    });
+  }
+
+  function onResetAccounts() {
+    setAccounts([]);
   }
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <span className="logos">
-          <a href="https://nextjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={nextLogo}
-              className="logo next"
-              alt="Next logo"
-            />
-          </a>
-        </span>
-        <span className="logos">
-          <a href="https://tauri.app" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={tauriLogo}
-              className="logo tauri"
-              alt="Tauri logo"
-            />
-          </a>
-        </span>
-        <span className="logos">
-          <a href="https://reactjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={reactLogo}
-              className="logo react"
-              alt="React logo"
-            />
-          </a>
-        </span>
+      <div>
+        <p>Please drop csv</p>
+        <input type="file" onChange={(e) => onUploadFile(e)} />
       </div>
-
-      <p>Click on the Tauri, Next, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
+      <div>
+        <button disabled={isUploading || csvData === null} onClick={() => startImport()}>Import accounts</button>
       </div>
-
-      <p>{greetMsg}</p>
+      <div>
+        <button onClick={() => onResetAccounts()}>reset accounts</button>
+      </div>
+      <div>
+        <p>Accounts</p>
+        <ul>
+          {accounts.map((account) => (
+            <li key={account.url + account.username}>
+              {account.url} - {account.username}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
