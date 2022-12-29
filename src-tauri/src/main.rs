fc745645ecfdf0b2b1ab98f94066484f457a34ee
wada <wadayugo@keio.jp>
2022-12-29 18:13:22 +0900

    add states

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,21 @@
     windows_subsystem = "windows"
 )]
 
+use std::collections::HashMap;
+
+use tauri::Manager;
 use tauri_app::{
     __cmd__filter_accounts, __cmd__import_accounts, filter_accounts::filter_accounts,
-    import_accounts::import_accounts,
+    import_accounts::import_accounts, AccountPartial,
 };
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let accounts: HashMap<String, Vec<AccountPartial>> = HashMap::new();
+            app.manage(accounts);
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![import_accounts, filter_accounts])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
