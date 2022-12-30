f7c906cd410e0175d41e85cf720d43f65e816168
wada <wadayugo@keio.jp>
2022-12-30 16:16:38 +0900

    fix condition

--- src-tauri/src/filter_accounts.rs
+++ src-tauri/src/filter_accounts.rs
@@ -17,28 +17,34 @@ pub fn filter_accounts(
         "is_icloud_included: {}, is_chrome_included: {}, is_firefox_included: {}",
         is_icloud_included, is_chrome_included, is_firefox_included
     );
+    // print state
+    let accounts_map = accounts_map_state.lock().unwrap();
     // create HashMap: key is url, value is username and source
     let mut filtered_accounts_map: HashMap<String, Vec<AccountPartial>> = HashMap::new();
     // filter accounts
-    let accounts_map = accounts_map_state.lock().unwrap();
-    for (url, accounts) in accounts_map.iter() {
+    for (url, accounts_vec) in accounts_map.iter() {
         // create new Vec
         let mut new_accounts_vec = Vec::new();
-        for account in accounts {
+        for account_partial in accounts_vec {
             // check if source is included
-            if (account.source == "icloud" && is_icloud_included)
-                || (account.source == "chrome" && is_chrome_included)
-                || (account.source == "firefox" && is_firefox_included)
+            if (account_partial.source == "icloud" && is_icloud_included)
+                || (account_partial.source == "chrome" && is_chrome_included)
+                || (account_partial.source == "firefox" && is_firefox_included)
             {
                 new_accounts_vec.push(AccountPartial {
-                    username: account.username.as_str().to_string(),
-                    password: account.password.as_str().to_string(),
-                    source: account.source.as_str().to_string(),
+                    username: account_partial.username.as_str().to_string(),
+                    password: account_partial.password.as_str().to_string(),
+                    source: account_partial.source.as_str().to_string(),
                 });
             }
         }
-        // add to accounts_map
-        filtered_accounts_map.insert(url.to_string(), new_accounts_vec);
+        if new_accounts_vec.len() > 0 {
+            // add to filtered_accounts_map
+            filtered_accounts_map.insert(url.to_string(), new_accounts_vec);
+        } else {
+            // remove from filtered_accounts_map
+            filtered_accounts_map.remove(url);
+        }
     }
     // print result length
     println!(
