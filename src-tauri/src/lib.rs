a92b221c26be4aa2c5526c2683857b669ef55210
wada <wadayugo@keio.jp>
2022-12-30 15:36:46 +0900

    update backend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,17 +4,23 @@ pub mod import;
 pub mod import_accounts;
 pub mod utils;
 
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Account {
     pub url: String,
     pub username: String,
+    password: String,
     pub source: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AccountPartial {
     pub username: String,
+    password: String,
     pub source: String,
 }
+
+pub type AccountMap = HashMap<String, Vec<AccountPartial>>;
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,19 +3,19 @@
     windows_subsystem = "windows"
 )]
 
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Mutex};
 
 use tauri::Manager;
 use tauri_app::{
     __cmd__filter_accounts, __cmd__import_accounts, filter_accounts::filter_accounts,
-    import_accounts::import_accounts, AccountPartial,
+    import_accounts::import_accounts, AccountMap,
 };
 
 fn main() {
     tauri::Builder::default()
         .setup(|app| {
-            let accounts: HashMap<String, Vec<AccountPartial>> = HashMap::new();
-            app.manage(accounts);
+            let accounts_map_state: Mutex<AccountMap> = Mutex::new(HashMap::new());
+            app.manage(accounts_map_state);
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![import_accounts, filter_accounts])
